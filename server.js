ff0fbf6064a2be40e293f83c7d746e1930ceda8e
Michael <mmbmf.mm@gmail.com>
2019-12-06 07:54:33 -0600

    final code

--- server.js
+++ server.js
@@ -1,12 +1,54 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+const MOVIES = require('./movies-data-small.json')
+
+// console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(cors())
+app.use(helmet())
 
-app.use((req, res) => {
-    res.send('Hello World')
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.query.Authorization
+    //const authoToken = req.get('Authorization') DOES NOT WORK
+
+    // console.log(authToken)
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+
+    next()
+})
+
+app.get('/movie', function handleGetMovies(req, res) {
+    let response = MOVIES
+
+    if(req.query.genre) {
+        response = response.filter(movie =>
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+        )
+    }
+
+    if(req.query.country) {
+        response = response.filter(movie =>
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+        )
+    }
+
+    if(req.query.avg_vote) {
+        response = response.filter(movie =>
+            Number(movie.avg_vote >= Number(req.query.avg_vote))
+        )
+    }
+
+    res.json(response)
 })
 
 const PORT = 8000;
