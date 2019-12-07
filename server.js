8c756fec4130bc351bc85e58ce3d0b7f3b19a091
Michael <mmbmf.mm@gmail.com>
2019-12-07 12:04:43 -0600

    minor changes after deploy

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const helmet = require("helmet");
 const MOVIES = require("./movies-data-small.json");
 
 const app = express();
-const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
 
-app.use(morgan(morganSetting))
+app.use(morgan(morganSetting));
 app.use(cors());
 app.use(helmet());
 
@@ -45,6 +45,16 @@ app.get("/movie", function handleGetMovies(req, res) {
   }
 
   res.json(response);
+});
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
 });
 
 const PORT = process.env.PORT || 8000;
