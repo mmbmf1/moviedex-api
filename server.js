24e060c3e43ecbb7e73424914e72d1b15af0f6ba
Michael <mmbmf.mm@gmail.com>
2019-12-07 10:46:13 -0600

    added authorization header

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ app.use(helmet());
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
-  const authToken = req.query.Authorization;
-  //const authoToken = req.get('Authorization') DOES NOT WORK
-
-  // console.log(authToken)
+  const authToken = req.get('Authorization');
 
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized request" });
